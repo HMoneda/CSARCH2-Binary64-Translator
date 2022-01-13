c62ba5166e5c2e0fc73ae2142eb61c20bb94cacb
HMoneda <80668451+HMoneda@users.noreply.github.com>
2022-01-13 21:14:06 +0800

    Added: QuasarJS

---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <q-toolbar class="bg-purple text-white shadow-2"
+      ><q-btn flat label="Binary 64 Translator" :to="{ name: 'Home' }"
+    /></q-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+};
+</script>
+
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+
 import router from './router';
 import store from './store';
+
 import './styles/index.css';
 
-createApp(App).use(router).use(store).mount('#app');
+import { Quasar } from 'quasar';
+
+import '@quasar/extras/roboto-font/roboto-font.css';
+import '@quasar/extras/material-icons/material-icons.css';
+
+import 'quasar/src/css/index.sass';
+
+const app = createApp(App);
+
+app.use(Quasar);
+app.use(router);
+app.use(store);
+
+app.mount('#app');
